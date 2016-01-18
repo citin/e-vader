aed66813c7a26a37e17d7f9185f5fc9019e170ca
Andres <cito.bernardi@gmail.com>
2016-01-18 16:39:27 -0300

    adds clients controller and routes

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -15,6 +15,7 @@ class ClientsController < ApplicationController
   # GET /clients/new
   def new
     @client = Client.new
+    @client.contacts.build
   end
 
   # GET /clients/1/edit
@@ -25,7 +26,6 @@ class ClientsController < ApplicationController
   # POST /clients.json
   def create
     @client = Client.new(client_params)
-
     respond_to do |format|
       if @client.save
         format.html { redirect_to @client, notice: 'Client was successfully created.' }
@@ -69,6 +69,6 @@ class ClientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
-      params.require(:client).permit(:name, :surname, :birthdate, :gender, :du, :cui)
+      params.require(:client).permit(:name, :surname, :birthdate, :gender, :du, :cui, contacts_attributes: [:id, :kind, :info])
     end
 end
