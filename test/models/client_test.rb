84be996c3cf0d663425177a79ea8ff21458391e2
Andres <cito.bernardi@gmail.com>
2016-02-07 16:43:27 -0300

    adds more client tests

--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -20,5 +20,24 @@ class ClientTest < ActiveSupport::TestCase
     assert_not cli3.valid?
   end
 
+  test "Borrado de cliente CON factura" do
+    params = { client: { surname: 'Sampayo', name: 'Ricardo', birthdate: Time.zone.parse("1988-05-09"), gender: 'masculino', du: '87552',cui:'3-3445-2', contacts_attributes: [{kind: 'whasap', info: '54221876492'}] } }
+    cli4 = Client.create(params[:client])
+
+    bill = Bill.create(description: 'Factura 1',
+                   total: '100',
+                   issue_date: Time.zone.parse("1999-05-09"),
+                   person_id: 1,
+                   client_id: cli4.id )
+    cli4.destroy
+    assert_not Bill.find_by_id bill.id
+  end
+  test "Borrado de cliente SIN factura" do
+    params = { client: { surname: 'Sampayo', name: 'Ricardo', birthdate: Time.zone.parse("1988-05-09"), gender: 'masculino', du: '875522',cui:'3-34452-2', contacts_attributes: [{kind: 'whasap', info: '54221876492'}] } }
+    cli5 = Client.create(params[:client])
+    cli5.destroy
+    assert_not Client.find_by_id cli5.id
+  end
+
 
 end
